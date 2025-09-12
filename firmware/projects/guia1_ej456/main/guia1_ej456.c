cd5de2c37add68df01136bd692b2dfdc400cf5a2
CarmeDominguez <carmedominguez2003@gmail.com>
2025-09-12 15:22:40 -0300

    Proyecto 1 completado

--- firmware/projects/guia1_ej456/main/guia1_ej456.c
+++ firmware/projects/guia1_ej456/main/guia1_ej456.c
@@ -47,42 +47,23 @@
 
 /*==================[internal data definition]===============================*/
 
-void app_main(void){
-	
-	typedef struct  /* Define un tipo de dato nuevo llamado gpioConf_t.*/
-	{
-		gpio_t pin;			/*!guarda qué pin físico del micro vas a usar */
-		io_t dir;			/*!indica la dirección del pin: entrada (IN) o salida (OUT), '0' IN;  '1' OUT*/
-	} gpioConf_t;
-	
-	gpioConf_t b0 = {GPIO_20,GPIO_OUTPUT};
-	gpioConf_t b1 = {GPIO_21,GPIO_OUTPUT};
-	gpioConf_t b2 = {GPIO_22,GPIO_OUTPUT};
-	gpioConf_t b3 = {GPIO_23,GPIO_OUTPUT};
+typedef struct  /* Define un tipo de dato nuevo llamado gpioConf_t.*/
+{
+	gpio_t pin;			/*!guarda qué pin físico del micro vas a usar */
+	io_t dir;			/*!indica la dirección del pin: entrada (IN) o salida (OUT), '0' IN;  '1' OUT*/
+} gpioConf_t;
 
-	gpioConf_t vector_GPIO[4];
-	vector_GPIO[0]=b0;
-	vector_GPIO[1]=b1;
-	vector_GPIO[2]=b2;
-	vector_GPIO[3]=b3;
+void func_on_off(uint8_t bcd_number, gpioConf_t *vector_GPIO){
+	uint8_t i;
+	for (i = 0; i < 4; i++){
+        uint8_t bit_val = (bcd_number >> i) & 0x01;  // extraigo bit i
 
-
-	void func_on_off(uint8_t bcd_number, gpioConf_t *vector_GPIO){
-		uint8_t i;
-		for (i=0; i<4; i++){
-			
-		}
-
-	};
-
-
-};
-
-
-
-/*==================[internal functions declaration]=========================*/
-
-/*==================[external functions definition]==========================*/
+        if (bit_val)
+            GPIOOn(vector_GPIO[i].pin);   // setea en 1 el pin
+        else
+            GPIOOff(vector_GPIO[i].pin);  // setea en 0 el pin
+    }
+}
 
 /*BCD_to_Array recibe un numero entero, la cantidad de digitos de dicho entero y un puntero que apunta a un arreglo que voy a modificar*/
 
@@ -94,5 +75,66 @@ void BCD_to_Array (uint32_t int_ingresado, uint8_t cant_digitos, uint8_t *array_
 		int_ingresado = int_ingresado / 10; //elimino el dígito menos significativo
 };
 }
+
+void display (uint32_t dato, uint8_t cant_digitos, gpioConf_t *vector_GPIO, gpioConf_t *vector_LCD ){
+	uint8_t digitos[3] = {0};
+    BCD_to_Array(dato, cant_digitos, digitos);
+
+    for (uint8_t i = 0; i < cant_digitos; i++){
+        // Activar el dígito correspondiente del LCD
+        GPIOOn(vector_LCD[i].pin);
+
+        // Mandar el dígito a los pines GPIO
+        func_on_off(digitos[i], vector_GPIO);
+
+		vTaskDelay(pdMS_TO_TICKS(5));   // 5 ms por dígito
+
+        // Apagar el dígito antes de pasar al siguiente (multiplexado)
+        GPIOOff(vector_LCD[i].pin);
+    }
+
+}
+
+void app_main(void){
+
+	// Inicializo los pines
+	GPIOInit(GPIO_20, GPIO_OUTPUT);
+	GPIOInit(GPIO_21, GPIO_OUTPUT);
+	GPIOInit(GPIO_22, GPIO_OUTPUT);
+	GPIOInit(GPIO_23, GPIO_OUTPUT);
+	GPIOInit(GPIO_19, GPIO_OUTPUT);
+	GPIOInit(GPIO_18, GPIO_OUTPUT);
+	GPIOInit(GPIO_9,  GPIO_OUTPUT);
+
+	gpioConf_t b0 = {GPIO_20,GPIO_OUTPUT};
+	gpioConf_t b1 = {GPIO_21,GPIO_OUTPUT};
+	gpioConf_t b2 = {GPIO_22,GPIO_OUTPUT};
+	gpioConf_t b3 = {GPIO_23,GPIO_OUTPUT};
+
+	// Defino el mapeo b0 → GPIO_20, b1 → GPIO_21, etc.
+	gpioConf_t vector_GPIO[4];
+	vector_GPIO[0]=b0;
+	vector_GPIO[1]=b1;
+	vector_GPIO[2]=b2;
+	vector_GPIO[3]=b3;
+
+	gpioConf_t dig1 = {GPIO_19,GPIO_OUTPUT};
+	gpioConf_t dig2 = {GPIO_18,GPIO_OUTPUT};
+	gpioConf_t dig3 = {GPIO_9,GPIO_OUTPUT};
+
+	// Defino el mapeo Digito 1 → GPIO_19, Digito 2 → GPIO_18, Digito 3 → GPIO_9.
+	gpioConf_t vector_LCD[3];
+	vector_LCD[0]=dig1;
+	vector_LCD[1]=dig2;
+	vector_LCD[2]=dig3;
+
+	while(1){
+		display(678, 3, vector_GPIO, vector_LCD);
+	}
+}
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
 
 /*==================[end of file]============================================*/
